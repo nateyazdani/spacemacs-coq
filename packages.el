5d52d5a3565acc4218b4f33ba49f56e3d45f4904
Tej Chajed <tchajed@mit.edu>
2017-11-18 14:15:01 -0500

    Re-indent according to Emacs lisp auto-indent

--- packages.el
+++ packages.el
@@ -12,14 +12,14 @@
 ;;; License: GPLv3
 
 (setq coq-packages
-    '(
-      company-coq
-              (proof-general :location (recipe
+      '(
+        company-coq
+        (proof-general :location (recipe
                                   :fetcher github
                                   :repo "ProofGeneral/PG"
                                   :files ("*")))
 
-      ))
+        ))
 
 (setq coq-excluded-packages '())
 
@@ -50,8 +50,8 @@
     (add-hook 'coq-mode-hook #'setup-coq-keys)
     (add-hook 'coq-mode-hook #'hide-mode-statuses)
     (spacemacs/declare-prefix-for-mode
-     'coq-mode
-     "mi" "coq/insert")
+      'coq-mode
+      "mi" "coq/insert")
     (spacemacs/set-leader-keys-for-major-mode 'coq-mode
       "il" 'company-coq-lemma-from-goal
       "im" 'company-coq-insert-match-construct)))
@@ -61,7 +61,7 @@
   ;; Setup from Proof General README, using a Homebrew path. Proof General
   ;; lazily loads from proof-site, so there's no need to use-package it.
   ;; (load "/usr/local/share/emacs/site-lisp/proof-general/generic/proof-site")
-    (use-package proof-site
+  (use-package proof-site
     :mode ("\\.v\\'" . coq-mode)
     :defer t
     :init
@@ -73,55 +73,55 @@
             proof-script-fly-past-comments t
             proof-splash-seen t)
       (add-to-list 'load-path coq/proof-general-load-path))
-  (dolist (prefix '(("ml" . "pg/layout")
-                    ("mp" . "pg/prover")
-                    ("ma" . "pg/ask-prover")
-                    ("mai" . "show-implicits")
-                    ("man" . "show-all") ; n is for notation
-                    ("mg" . "pg/goto")))
-    (spacemacs/declare-prefix-for-mode
-     'coq-mode
-     (car prefix) (cdr prefix)))
-  (spacemacs/set-leader-keys-for-major-mode 'coq-mode
-    ;; Basic proof management
-    "]" 'proof-assert-next-command-interactive
-    "[" 'proof-undo-last-successful-command
-    "." 'proof-goto-point
-    ;; Layout
-    "ll" 'proof-layout-windows
-    "lc" 'pg-response-clear-displays
-    "lp" 'proof-prf
-    ;; Prover Interaction
-    "px" 'proof-shell-exit
-    "pc" 'proof-interrupt-process
-    "pr" 'proof-retract-buffer
-    "pb" 'proof-process-buffer
-    ;; Prover queries ('ask prover')
-    "af" 'proof-find-theorems
-    "ap" 'coq-Print
-    "ac" 'coq-Check
-    "ab" 'coq-About
-    "aip" 'coq-Print-with-implicits
-    "aic" 'coq-Check-show-implicits
-    "aib" 'coq-About-with-implicits
-    "anp" 'coq-Print-with-all
-    "anc" 'coq-Check-show-all
-    "anb" 'coq-About-with-all
-    ;; Moving the point (goto)
-    "g." 'proof-goto-end-of-locked
-    "ga" 'proof-goto-command-start
-    "ge" 'proof-goto-command-end
-    ;; Insertions
-    "ie" 'coq-end-Section)
-  ;; Workaround for evil performance bug (see
-  ;; https://github.com/olivierverdier/spacemacs-coq/issues/6 for details).
-  ;; Essentially the cursor color is changed rapidly when navigating the proof
-  ;; in insert mode. This ensures the insert and normal mode colors are the
-  ;; same, which generally avoids this problem.
-  (add-hook 'coq-mode-hook
-            (lambda ()
-              (setq-local
-               evil-insert-state-cursor
-               (cons
-                (car evil-normal-state-cursor)
-                (cdr evil-insert-state-cursor)))))))
+    (dolist (prefix '(("ml" . "pg/layout")
+                      ("mp" . "pg/prover")
+                      ("ma" . "pg/ask-prover")
+                      ("mai" . "show-implicits")
+                      ("man" . "show-all") ; n is for notation
+                      ("mg" . "pg/goto")))
+      (spacemacs/declare-prefix-for-mode
+        'coq-mode
+        (car prefix) (cdr prefix)))
+    (spacemacs/set-leader-keys-for-major-mode 'coq-mode
+      ;; Basic proof management
+      "]" 'proof-assert-next-command-interactive
+      "[" 'proof-undo-last-successful-command
+      "." 'proof-goto-point
+      ;; Layout
+      "ll" 'proof-layout-windows
+      "lc" 'pg-response-clear-displays
+      "lp" 'proof-prf
+      ;; Prover Interaction
+      "px" 'proof-shell-exit
+      "pc" 'proof-interrupt-process
+      "pr" 'proof-retract-buffer
+      "pb" 'proof-process-buffer
+      ;; Prover queries ('ask prover')
+      "af" 'proof-find-theorems
+      "ap" 'coq-Print
+      "ac" 'coq-Check
+      "ab" 'coq-About
+      "aip" 'coq-Print-with-implicits
+      "aic" 'coq-Check-show-implicits
+      "aib" 'coq-About-with-implicits
+      "anp" 'coq-Print-with-all
+      "anc" 'coq-Check-show-all
+      "anb" 'coq-About-with-all
+      ;; Moving the point (goto)
+      "g." 'proof-goto-end-of-locked
+      "ga" 'proof-goto-command-start
+      "ge" 'proof-goto-command-end
+      ;; Insertions
+      "ie" 'coq-end-Section)
+    ;; Workaround for evil performance bug (see
+    ;; https://github.com/olivierverdier/spacemacs-coq/issues/6 for details).
+    ;; Essentially the cursor color is changed rapidly when navigating the proof
+    ;; in insert mode. This ensures the insert and normal mode colors are the
+    ;; same, which generally avoids this problem.
+    (add-hook 'coq-mode-hook
+              (lambda ()
+                (setq-local
+                 evil-insert-state-cursor
+                 (cons
+                  (car evil-normal-state-cursor)
+                  (cdr evil-insert-state-cursor)))))))
