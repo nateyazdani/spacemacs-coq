7167a3418352d0c72bad3888a2fdfe5fdf402012
Nate Yazdani <nyazdani@cs.washington.edu>
2019-11-12 16:29:17 -0500

    My new-and-improved spacemacs layer for Coq
    Builds on the work of several others (see comment header).

--- packages.el
+++ packages.el
@@ -1,11 +1,14 @@
-;;; packages.el --- coq Layer packages File for Spacemacs
+;;; packages.el --- Coq Layer Packages for Spacemacs
 ;;
+;; Copyright (c) 2019 Nathaniel Yazdani
 ;; Copyright (c) 2016 Tej Chajed
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
 ;;
-;; Based on original version by Olivier Verdier
+;; Based on modified version by Tej Chajed.
+;; Based on original version by Olivier Verdier.
 ;;
-;; Author: Tej Chajed <tchajed@mit.edu>
-;; URL: https://github.com/tchajed/spacemacs-coq
+;; Author: Nathaniel Yazdani <nyazdani@ccs.neu.edu>
+;; URL: https://github.com/nateyazdani/spacemacs-coq
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -13,121 +16,159 @@
 
 (setq coq-packages
       '(
+        proof-general
         company-coq
-        (proof-general :location (recipe
-                                  :fetcher github
-                                  :repo "ProofGeneral/PG"
-                                  :files ("*")))
-
         ))
 
 (setq coq-excluded-packages '())
 
-(defun setup-coq-keys-for-map (state)
-  (evil-define-key state coq-mode-map
-    (kbd "<f3>") #'proof-assert-next-command-interactive)
-  (evil-define-key state coq-mode-map
-    (kbd "<f4>") #'company-coq-proof-goto-point)
-  (evil-define-key state coq-mode-map
-    (kbd "<f2>") #'proof-undo-last-successful-command))
-
-(defun setup-coq-keys ()
-  (setup-coq-keys-for-map 'normal)
-  (setup-coq-keys-for-map 'insert))
-
-(defun hide-mode-statuses ()
-  (diminish 'company-mode)
-  (diminish 'hs-minor-mode)
-  (diminish 'outline-minor-mode)
-  (diminish 'company-mode)
-  (diminish 'holes-mode))
+(defvar coq/showing-implicits nil)
 
 (defun coq/init-company-coq ()
+  "Initialize Company Coq upon entering coq-mode."
   (use-package company-coq
     :defer t
     :init
-    (add-hook 'coq-mode-hook #'company-coq-mode)
-    (add-hook 'coq-mode-hook #'setup-coq-keys)
-    (add-hook 'coq-mode-hook #'hide-mode-statuses)
-    (spacemacs/declare-prefix-for-mode
-      'coq-mode
-      "mi" "coq/insert")
-    (spacemacs/set-leader-keys-for-major-mode 'coq-mode
-      "il" 'company-coq-lemma-from-goal
-      "im" 'company-coq-insert-match-construct)))
+    (progn
+      (add-hook 'coq-mode-hook #'company-coq-mode)
+      (setq company-coq-disabled-features '(hello keybindings snippets)))
+    :config
+    (progn
+      (spacemacs/declare-prefix-for-mode 'coq-mode "mx" "source")
+      (spacemacs/set-leader-keys-for-major-mode 'coq-mode
+        "." #'company-coq-proof-goto-point
+        "=" #'company-coq-eval-last-sexp
+
+        ;; Source manipulation (text).
+        "x-" #'company-coq-fold
+        "x+" #'company-coq-unfold
+        "x*" #'company-coq-toggle-definition-overlay
+        "x/" #'company-coq-grep-symbol
+        "xo" #'company-coq-occur
+        "xg" #'company-coq-goto-occurence
+        "xd" #'company-coq-diff-dwim
+        "xj" #'company-coq-jump-to-definition
+
+        ;; Documentation retrieval.
+        "d" #'company-coq-doc
+
+        ;; Prover help.
+        "?" #'company-coq-document-error
+        ))))
 
 (defun coq/init-proof-general ()
   "Initialize Proof General."
-  ;; Setup from Proof General README, using a Homebrew path. Proof General
-  ;; lazily loads from proof-site, so there's no need to use-package it.
-  ;; (load "/usr/local/share/emacs/site-lisp/proof-general/generic/proof-site")
   (use-package proof-site
-    :mode ("\\.v\\'" . coq-mode)
     :defer t
     :init
     (progn
       (setq coq/proof-general-load-path
             (concat (configuration-layer/get-elpa-package-install-directory
-                     'proof-general) "generic")
-            proof-three-window-mode-policy 'hybrid
-            proof-script-fly-past-comments t
-            proof-splash-seen t)
-      (add-to-list 'load-path coq/proof-general-load-path))
-    (dolist (prefix '(("ml" . "pg/layout")
-                      ("mp" . "pg/prover")
-                      ("ma" . "pg/ask-prover")
-                      ("mai" . "show-implicits")
-                      ("man" . "show-all") ; n is for notation
-                      ("mg" . "pg/goto")))
-      (spacemacs/declare-prefix-for-mode
-        'coq-mode
-        (car prefix) (cdr prefix)))
-    (spacemacs/set-leader-keys-for-major-mode 'coq-mode
-      ;; Basic proof management
-      "]" 'proof-assert-next-command-interactive
-      "[" 'proof-undo-last-successful-command
-      "." 'proof-goto-point
-      ;; Layout
-      "ll" 'proof-layout-windows
-      "lc" 'pg-response-clear-displays
-      "lp" 'proof-prf
-      ;; Prover Interaction
-      "px" 'proof-shell-exit
-      "pc" 'proof-interrupt-process
-      "pr" 'proof-retract-buffer
-      "pb" 'proof-process-buffer
-      ;; Prover queries ('ask prover')
-      "af" 'proof-find-theorems
-      "ap" 'coq-Print
-      "ac" 'coq-Check
-      "ab" 'coq-About
-      "aip" 'coq-Print-with-implicits
-      "aic" 'coq-Check-show-implicits
-      "aib" 'coq-About-with-implicits
-      "anp" 'coq-Print-with-all
-      "anc" 'coq-Check-show-all
-      "anb" 'coq-About-with-all
-      ;; Moving the point (goto)
-      "g." 'proof-goto-end-of-locked
-      "ga" 'proof-goto-command-start
-      "ge" 'proof-goto-command-end
-      ;; Insertions
-      "ie" 'coq-end-Section)
-    ;; Workaround for evil performance bug (see
-    ;; https://github.com/olivierverdier/spacemacs-coq/issues/6 for details).
-    ;; Essentially the cursor color is changed rapidly when navigating the proof
-    ;; in insert mode. This ensures the insert and normal mode colors are the
-    ;; same, which generally avoids this problem.
-    (add-hook 'coq-mode-hook
-              (lambda ()
-                (setq-local
-                 evil-insert-state-cursor
-                 (cons
-                  (car evil-normal-state-cursor)
-                  (cdr evil-insert-state-cursor)))))
-    (add-hook 'coq-mode-hook
-              (lambda ()
-                (purpose-add-user-purposes
-                 :modes
-                 '((proof-response-mode . response)
-                   (proof-goals-mode . goals)))))))
+                     'proof-general)
+                    "generic"))
+      (add-to-list 'load-path coq/proof-general-load-path)
+      (setq proof-three-window-mode-policy 'hybrid
+            proof-keep-response-history t
+            proof-output-tooltips t
+            proof-toolbar-enable nil
+            proof-splash-enable nil))
+    :config
+    (progn
+      (defun proof-search-location ()
+        (interactive)
+        (funcall-interactively #'coq-LocateConstant))
+      (defun proof-search-notation ()
+        (interactive)
+        (funcall-interactively #'coq-LocateNotation))
+      (defun proof-search-rewrite ()
+        (interactive)
+        (funcall-interactively #'coq-SearchRewrite))
+      (defun proof-search-about ()
+        (interactive)
+        (funcall-interactively #'coq-SearchConstant))
+      (defun proof-search-pattern ()
+        (interactive)
+        (funcall-interactively #'coq-SearchIsos))
+
+      (defun proof-toggle-showing-implicits ()
+        (interactive)
+        (setq coq/showing-implicits (not coq/showing-implicits)))
+
+      (defun proof-print ()
+        (interactive)
+        (if coq/showing-implicits
+            (funcall-interactively #'coq-Print)
+          (funcall-interactively #'coq-Print-with-implicits)))
+      (defun proof-check ()
+        (interactive)
+        (if coq/showing-implicits
+            (funcall-interactively #'coq-Check)
+          (funcall-interactively #'coq-Check-show-implicits)))
+      (defun proof-about ()
+        (interactive)
+        (if coq/showing-implicits
+            (funcall-interactively #'coq-About)
+          (funcall-interactively #'coq-About-with-implicits)))
+
+      (defun proof-print-full ()
+        (interactive)
+        (funcall-interactively #'coq-Print-with-all))
+      (defun proof-check-full ()
+        (interactive)
+        (funcall-interactively #'coq-Check-show-all))
+      (defun proof-about-full ()
+        (interactive)
+        (funcall-interactively #'coq-About-with-all))
+
+      (spacemacs/declare-prefix-for-mode 'coq-mode "ml" "layout")
+      (spacemacs/declare-prefix-for-mode 'coq-mode "mp" "prover")
+      (spacemacs/declare-prefix-for-mode 'coq-mode "ms" "search")
+      (spacemacs/declare-prefix-for-mode 'coq-mode "mi" "inspect")
+      (spacemacs/declare-prefix-for-mode 'coq-mode "mg" "goto")
+
+      (spacemacs/set-leader-keys-for-major-mode 'coq-mode
+        ;; Prover interaction.
+        "]" #'proof-assert-next-command-interactive
+        "[" #'proof-undo-last-successful-command
+        "/" #'proof-find-theorems ; same as #'coq-SearchConstant
+
+        ;; Window layout
+        "ll" #'proof-layout-windows
+        "lt" #'proof-tree-external-display-toggle
+        "lr" #'proof-display-some-buffers
+        "lc" #'pg-response-clear-displays
+        "lp" #'proof-prf
+
+        ;; Prover management.
+        "pc" #'proof-ctxt
+        "ps" #'proof-toggle-active-scripting
+        "pb" #'proof-process-buffer
+        "pr" #'proof-retract-buffer
+        "pk" #'proof-interrupt-process
+        "pq" #'proof-shell-exit
+
+        ;; Context search.
+        "sl" #'proof-search-location
+        "sn" #'proof-search-notation
+        "sr" #'proof-search-rewrite
+        "sp" #'proof-search-pattern
+        "sa" #'proof-search-about
+
+        ;; Context inspection.
+        "c" #'proof-check
+        "a" #'proof-about
+        "ip" #'proof-print
+        "iP" #'proof-print-full
+        "ic" #'proof-check
+        "iC" #'proof-check-full
+        "ia" #'proof-about
+        "iA" #'proof-about-full
+        "i!" #'proof-toggle-showing-implicits
+
+        ;; Point motion (goto).
+        "g." #'proof-goto-end-of-locked
+        "g0" #'proof-goto-command-start
+        "g$" #'proof-goto-command-end
+        "gb" #'proof-backward-command
+        "gw" #'proof-forward-command
+        ))))
